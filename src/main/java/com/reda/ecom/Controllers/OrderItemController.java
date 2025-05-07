5919a0b8cd5a6c4005c03908386157f8cdbe60c2
sayah-reda <reda1998.rs@gmail.com>
2025-05-07 14:18:15 +0100

    feat: add Controllers

---
+++ src/main/java/com/reda/ecom/Controllers/OrderItemController.java
@@ -0,0 +1,10 @@
+package com.reda.ecom.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orderItems")
+public class OrderItemController {
+
+}
---
+++ src/main/java/com/reda/ecom/Controllers/RestaurentController.java
@@ -0,0 +1,10 @@
+package com.reda.ecom.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/restaurants")
+public class RestaurentController {
+
+}
